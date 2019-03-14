d06f5c0216edee4a4507b5245e84ad3ae5832add
Fola Orokunle <robingoodfellow17@users.noreply.github.com>
2019-03-14 13:59:24 -0400

    master transfer

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./Header.css";
-import logo from './images/logo4.png'
+import logo from './images/logo.svg'
 
 
 class Header extends Component {
@@ -19,21 +19,28 @@ class Header extends Component {
     // </div>  
 
     <div class="header cover bg-left bg-center-l" >
-        <div class="bg-black-20 pb5 pb6-m pb7-l">
-        <nav class="dt  mw8 center"> 
-    
-        <div className="w-100 dtc v-mid tr pa3">
-        <Link className="f3 fw5 hover-blue no-underline white-70 dn dib-l pv2 ph3" to="/home">Home</Link> 
-        <Link className="f3 fw5 hover-red no-underline white-70 dn dib-l pv2 ph3" to="/about">About</Link> 
-        <Link className="f3 fw5 hover-blue no-underline white-70 dn dib-l pv2 ph3" to="/contact">Contact</Link>
-        <Link className="f3 fw5 hover-red no-underline white-70 dn dib-l pv2 ph3" to="/signup">Sign Up</Link>
-        <Link className="f3 fw hover-blue no-underline white-70 dib ml2 pv2 ph3 ba" to="/login">Login</Link >
-        </div>
-    </nav> 
-    <div className="tc-l mt4 mt5-m mt6-l ph3">
-        <h1 className="f4 f1-l fw2 white-90 mb0 lh-title">Jump Shot Buddy</h1>
-        <h2 className="fw3 f3 white-80 mt3 mb4">Master Your Stroke.</h2>
+    <div class="bg-black-20 pb5 pb6-m pb7-l">
+      <nav class="dt w-100 mw8 center"> 
+        <div class="dtc w2 v-mid pa3">
+          <a href="/" class="dib w2 h2 pa1 ba b--white-90 grow-large border-box">
+          <img src="http://tachyons.io/img/logo.jpg" class="dibw2 h2 br-100" alt="Site Name"></img>
+          </a>
     </div>
+    <div className="dtc v-mid tr pa3">
+    <Link className="f6 fw4 hover-white no-underline white-70 dn dib-l pv2 ph3" to="/home">Home</Link> 
+    <Link className="f6 fw4 hover-white no-underline white-70 dn dib-l pv2 ph3" to="/about">About</Link> 
+    <Link className="f6 fw4 hover-white no-underline white-70 dn dib-l pv2 ph3" to="/contact">Contact</Link>
+    <Link className="f6 fw4 hover-white no-underline white-70 dn dib-l pv2 ph3" to="/signup">Sign Up</Link>
+    <Link className="f6 fw4 hover-white no-underline white-70 dib ml2 pv2 ph3 ba" to="/login">Login</Link >
+    </div>
+  </nav> 
+  <div className="tc-l mt4 mt5-m mt6-l ph3">
+    <h1 className="f2 f1-l fw2 white-90 mb0 lh-title">Jump Shot Buddy</h1>
+    <h2 className="fw1 f3 white-80 mt3 mb4">Master Your Stroke</h2>
+    <a className="f6 no-underline grow dib v-mid bg-blue white ba b--blue ph3 pv2 mb3" href="/">Call to Action</a>
+    <span className="dib v-mid ph3 white-70 mb3">or</span>
+    <a className="f6 no-underline grow dib v-mid white ba b--white ph3 pv2 mb3" href="">Secondary call to action</a>
+  </div>
 </div>
 </div> 
     );
