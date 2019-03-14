ccbe328271336ecf04d55db8693a2036b9340c52
Fola Orokunle <robingoodfellow17@users.noreply.github.com>
2019-03-14 10:54:15 -0400

    error changes

--- src/Feature.js
+++ src/Feature.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {  MDBRow, MDBCol, MDBIcon } from "mdbreact";
+import Ball from './images/basketball.png'
 
 const FeaturesPage = () => {
   return (
@@ -23,7 +24,7 @@ const FeaturesPage = () => {
                   className="deep-purple-text"
                 />
               </MDBCol>
-              <MDBCol size="10">
+              <MDBCol size="6">
                 <h5 className="font-weight-bold mb-3">International</h5>
                 <p className="grey-text">
                   Bullet 2 ipsum dolor sit amet, consectetur adipisicing elit.
@@ -36,7 +37,7 @@ const FeaturesPage = () => {
               <MDBCol size="2">
                 <MDBIcon icon="flask" size="2x" className="deep-purple-text" />
               </MDBCol>
-              <MDBCol size="10">
+              <MDBCol size="8">
                 <h5 className="font-weight-bold mb-3">Experimental</h5>
                 <p className="grey-text">
                   Bullet 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -49,7 +50,7 @@ const FeaturesPage = () => {
               <MDBCol size="2">
                 <MDBIcon icon="glass-martini" size="2x" className="deep-purple-text" />
               </MDBCol>
-              <MDBCol size="10">
+              <MDBCol size="8">
                 <h5 className="font-weight-bold mb-3">Relaxing</h5>
                 <p className="grey-text">
                   Bullet 4 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -59,10 +60,10 @@ const FeaturesPage = () => {
               </MDBCol>
             </MDBRow>
           </MDBCol>
-          <MDBCol md="4" className="text-name">
+          <MDBCol md="4" className="text-center">
             <img
               className="img-fluid tc"
-              src="https://mdbootstrap.com/img/Mockups/Transparent/Small/iphone-portfolio1.png"
+              src={Ball}
               alt=""
             />
           </MDBCol>
@@ -71,7 +72,7 @@ const FeaturesPage = () => {
               <MDBCol size="2">
                 <MDBIcon icon="heart" size="2x" className="deep-purple-text" />
               </MDBCol>
-              <MDBCol size="10">
+              <MDBCol size="8">
                 <h5 className="font-weight-bold mb-3">Beloved</h5>
                 <p className="grey-text">
                   Bullet 5 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -84,7 +85,7 @@ const FeaturesPage = () => {
               <MDBCol size="2">
                 <MDBIcon icon="bolt" size="2x" className="deep-purple-text" />
               </MDBCol>
-              <MDBCol size="10">
+              <MDBCol size="8">
                 <h5 className="font-weight-bold mb-3">Rapid</h5>
                 <p className="grey-text">
                   Bullet 6 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -97,7 +98,7 @@ const FeaturesPage = () => {
               <MDBCol size="2">
                 <MDBIcon icon="magic" size="2x" className="deep-purple-text" />
               </MDBCol>
-              <MDBCol size="10">
+              <MDBCol size="8">
                 <h5 className="font-weight-bold mb-3">Magical</h5>
                 <p className="grey-text">
                   Bullet 7 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
