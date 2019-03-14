7dabc818d52a284b996c0f8ec899a87f69650a50
Fola Orokunle <robingoodfellow17@users.noreply.github.com>
2019-03-14 07:44:13 -0400

    update feature text

--- src/Feature.js
+++ src/Feature.js
@@ -8,7 +8,7 @@ const FeaturesPage = () => {
           Why is it so great?
         </h2>
         <p className="lead grey-text w-responsive text-center mx-auto mb-5">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          Bullet 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
           ad minim veniam.
         </p>
@@ -26,7 +26,7 @@ const FeaturesPage = () => {
               <MDBCol size="10">
                 <h5 className="font-weight-bold mb-3">International</h5>
                 <p className="grey-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Bullet 2 ipsum dolor sit amet, consectetur adipisicing elit.
                   Reprehenderit maiores nam, aperiam minima assumenda deleniti
                   hic.
                 </p>
@@ -39,7 +39,7 @@ const FeaturesPage = () => {
               <MDBCol size="10">
                 <h5 className="font-weight-bold mb-3">Experimental</h5>
                 <p className="grey-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Bullet 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                   Reprehenderit maiores nam, aperiam minima assumenda deleniti
                   hic.
                 </p>
@@ -52,7 +52,7 @@ const FeaturesPage = () => {
               <MDBCol size="10">
                 <h5 className="font-weight-bold mb-3">Relaxing</h5>
                 <p className="grey-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Bullet 4 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                   Reprehenderit maiores nam, aperiam minima assumenda deleniti
                   hic.
                 </p>
@@ -74,7 +74,7 @@ const FeaturesPage = () => {
               <MDBCol size="10">
                 <h5 className="font-weight-bold mb-3">Beloved</h5>
                 <p className="grey-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Bullet 5 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                   Reprehenderit maiores nam, aperiam minima assumenda deleniti
                   hic.
                 </p>
@@ -87,7 +87,7 @@ const FeaturesPage = () => {
               <MDBCol size="10">
                 <h5 className="font-weight-bold mb-3">Rapid</h5>
                 <p className="grey-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Bullet 6 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                   Reprehenderit maiores nam, aperiam minima assumenda deleniti
                   hic.
                 </p>
@@ -100,7 +100,7 @@ const FeaturesPage = () => {
               <MDBCol size="10">
                 <h5 className="font-weight-bold mb-3">Magical</h5>
                 <p className="grey-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Bullet 7 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                   Reprehenderit maiores nam, aperiam minima assumenda deleniti
                   hic.
                 </p>
