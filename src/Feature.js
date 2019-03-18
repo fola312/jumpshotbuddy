ce01b59da3f3f127fa4bb8baa49bae7f1614c9e1
Fola Orokunle <robingoodfellow17@users.noreply.github.com>
2019-03-18 08:59:34 -0400

    ball icon grid/flex setup

--- src/Feature.js
+++ src/Feature.js
@@ -1,11 +1,11 @@
 import React from "react";
 import {  MDBRow, MDBCol, MDBIcon } from "mdbreact";
-import ball from './images/ballicon2.png';
+import Player from './images/shootingb.jpg';
 
 const FeaturesPage = () => {
   return (
-    <section className="my-6 bc-blue">
-      <div >
+    <section className="my-6 rgba-white-light mb-5 mt-0">
+      <div className=" pt-0 mt-0 my-0">
         <h2 className="h1-responsive font-weight-bold text-center my-3">
           Hi, my Name is Fola. And my jump-shot sucks. 
         </h2>
@@ -14,19 +14,21 @@ const FeaturesPage = () => {
           basketall. Track shot-types, locations, FG% (field goal), and TS% ("true shooting").
         </p>
       </div>
-        
-
-        <MDBRow>
-          <MDBCol md="4">
+      <MDBRow>
+          <MDBCol md="6">
+          <MDBRow>ffff </MDBRow>
+          <MDBRow>ffff </MDBRow>
+          <MDBRow>ffff </MDBRow>
+          <MDBRow>ffff </MDBRow>
             <MDBRow className="mb-3 tc">
-              <MDBCol size="2">
+              <MDBCol size="4">
                 <MDBIcon
                   icon="check"
                   size="2x"
                   className="deep-purple-text"
                 />
               </MDBCol>
-              <MDBCol size="8">
+              <MDBCol size="6">
                 <h5 className="font-weight-bold mb-3">International</h5>
                 <p className="grey-text">
                   Bullet 2 ipsum dolor sit amet, consectetur adipisicing elit.
@@ -36,10 +38,10 @@ const FeaturesPage = () => {
               </MDBCol>
             </MDBRow>
             <MDBRow className="mb-3 tc">
-              <MDBCol size="2">
+              <MDBCol size="4">
                 <MDBIcon icon="check" size="2x" className="deep-purple-text" />
               </MDBCol>
-              <MDBCol size="8">
+              <MDBCol size="6">
                 <h5 className="font-weight-bold mb-3">Experimental</h5>
                 <p className="grey-text">
                   Bullet 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -49,10 +51,10 @@ const FeaturesPage = () => {
               </MDBCol>
             </MDBRow>
             <MDBRow className="mb-3 tc">
-              <MDBCol size="2">
+              <MDBCol size="4">
                 <MDBIcon icon="check" size="2x" className="deep-purple-text" />
               </MDBCol>
-              <MDBCol size="8">
+              <MDBCol size="6">
                 <h5 className="font-weight-bold mb-3">Relaxing</h5>
                 <p className="grey-text">
                   Bullet 4 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -61,52 +63,38 @@ const FeaturesPage = () => {
                 </p>
               </MDBCol>
             </MDBRow>
-          </MDBCol>
-          <MDBCol md="4" className="text-center">
-            <img
-              className="img-fluid tc"
-              src={ball}
-              alt=""
-            />
-          </MDBCol>
-          <MDBCol md="4">
             <MDBRow className="mb-3 tc">
-              <MDBCol size="2">
+              <MDBCol size="4">
                 <MDBIcon icon="check" size="2x" className="deep-purple-text" />
               </MDBCol>
-              <MDBCol size="8">
-                <h5 className="font-weight-bold mb-3">Beloved</h5>
-                <p className="grey-text">
-                  Bullet 5 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Reprehenderit maiores nam, aperiam minima assumenda deleniti
-                  hic.
-                </p>
-              </MDBCol>
-            </MDBRow>
-            <MDBRow className="mb-3 tc">
-              <MDBCol size="2">
-                <MDBIcon icon="check" size="2x" className="deep-purple-text" />
-              </MDBCol>
-              <MDBCol size="8">
+              <MDBCol size="6">
                 <h5 className="font-weight-bold mb-3">Rapid</h5>
                 <p className="grey-text">
-                  Bullet 6 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Reprehenderit maiores nam, aperiam minima assumenda deleniti
-                  hic.
+                true shooting percentage is an APBRmetrics statistic that measures a player's efficiency 
+                at shooting the ball. It is intended to more accurately calculate a player's shooting 
+                than field goal percentage, free throw percentage, and three-point field goal percentage 
+                taken individually
                 </p>
               </MDBCol>
             </MDBRow>
             <MDBRow className="mb-3 tc">
-              <MDBCol size="2">
+              <MDBCol size="4">
                 <MDBIcon icon="basketball-ball" size="2x" className="deep-purple-text" />
               </MDBCol>
-              <MDBCol size="8">
+              <MDBCol size="6">
                 <h5 className="font-weight-bold mb-3">Magical</h5>
                 <p className="grey-text">
                   Become the "TRUE HOOPER" no one ever thought you could be, because they dare not to dream.
                 </p>
               </MDBCol>
             </MDBRow>
+          </MDBCol>
+          <MDBCol md="6" className="text-center">
+            <img
+              className="img-fluid rounded-circle tc"
+              src={Player}
+              alt=""
+            />
           </MDBCol>
         </MDBRow>
       </section>
--- src/court.js
+++ src/court.js
@@ -1,14 +1,86 @@
 import React, { Component } from 'react';
-import Basketallc from './images/basketball.png';
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import Basketallc from './images/basketball.jpg';
+import { Button } from 'mdbreact';
+import Ballicon from './images/ball.jpg';
+import "./App.css";
 
 
 class Court extends Component {
   render() {
     return (
-      <div className='tc'>
-          <h1>Interactive basketball court</h1>
-          <img src={Basketallc} alt='basketballcourt' className='w-80'></img>
+    <MDBContainer fluid >
+        <MDBRow className='tc'>
+            <MDBCol>
+              <h1>happy Shooting! :)</h1> 
+              <Button color="primary" rounded>Hit it!</Button> 
+            </MDBCol>
+        </MDBRow>
+        <MDBRow></MDBRow>
+        <MDBRow></MDBRow>
+        <MDBRow></MDBRow>
+        <MDBRow></MDBRow>
+        <MDBRow></MDBRow>
+        <MDBRow></MDBRow>
+        <MDBRow></MDBRow>
+        <MDBRow>
+            <MDBCol>
+              <img src= {Ballicon} alt='ball icon' className='ballicons'></img>  
+            </MDBCol>
+            <MDBCol></MDBCol>
+            <MDBCol className='order-last'>
+              <img src= {Ballicon} alt='ball icon' className='ballicons'></img>  
+            </MDBCol>
+        </MDBRow>
+        <MDBRow>
+            <MDBCol>
+              <img src= {Ballicon} alt='ball icon' className='ballicons'></img>  
+            </MDBCol>
+        </MDBRow>
+        <MDBRow>
+            <MDBCol>
+              <img src= {Ballicon} alt='ball icon' className='ballicons'></img>  
+            </MDBCol>
+        </MDBRow>
+        <MDBRow>
+            <MDBCol>
+              <img src= {Ballicon} alt='ball icon' className='ballicons'></img>  
+            </MDBCol>
+        </MDBRow>
+        <MDBRow>
+            <MDBCol>
+              <img src= {Ballicon} alt='ball icon' className='ballicons'></img>  
+            </MDBCol>
+        </MDBRow>
+        <MDBRow>
+            <MDBCol>
+              <img src= {Ballicon} alt='ball icon' className='ballicons'></img>  
+            </MDBCol>
+        </MDBRow>
+        <MDBRow>
+            <MDBCol>
+              <img src= {Ballicon} alt='ball icon' className='ballicons'></img>  
+            </MDBCol>
+        </MDBRow>
+        <MDBRow>
+            <MDBCol>
+              <img src= {Ballicon} alt='ball icon' className='ballicons'></img>  
+            </MDBCol>
+        </MDBRow>
+                <div className='tc '>
+            
+                
+                <img src= {Ballicon} alt='ball icon' className='ballicons'></img>
+                <img src= {Ballicon} alt='ball icon' className='ballicons'></img>
+                <img src= {Ballicon} alt='ball icon' className='ballicons'></img>
+                <img src= {Ballicon} alt='ball icon' className='ballicons'></img>
+                <img src= {Ballicon} alt='ball icon' className='ballicons'></img>
+                <img src= {Ballicon} alt='ball icon' className='ballicons'></img>
+                <img src= {Ballicon} alt='ball icon' className='ballicons'></img>
+                <h1>balls</h1>
       </div>
+          </MDBContainer>
+                   /* <img src={Basketallc} alt='basketballcourt' className='w-80'></img> */
     );
   }
 }
