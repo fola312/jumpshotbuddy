e21f3eb154171442845ab9164ae0018b910da9ae
Fola Orokunle <robingoodfellow17@users.noreply.github.com>
2019-04-05 14:58:53 -0400

    header

--- src/Home.js
+++ src/Home.js
@@ -8,12 +8,12 @@ class Home extends Component {
           <h1>Tes Tes<div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> 
           from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by 
           <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" 
-          target="_blank">CC 3.0 BY</a></div></h1>
+          >CC 3.0 BY</a></div></h1>
           <h1><div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    
           title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    
-          title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div></h1>
+          title="Creative Commons BY 3.0">CC 3.0 BY</a>Designed by <a  href="https://www.vecteezy.com">www.vecteezy.com if I use flaming balls</a></div></h1>
 
-Designed by <a target="_blank" href="https://www.vecteezy.com">www.vecteezy.com if I use flaming balls</a>
+
       </div>
     );
   }
